719570ec206ada4547ba3cad438c68790bc97178
jude <judethomas0403@gmail.com>
2023-10-04 07:31:12 -0500

    more architecture

--- Code and Experiments/modules/qdanalysis/qdanalysis/models.py
+++ Code and Experiments/modules/qdanalysis/qdanalysis/models.py
@@ -6,13 +6,19 @@ import tensorflow as tf
 import tensorflow.keras as keras
 
 def adapt_resnet50(input_shape, freeze_weights=True):
-    input = keras.layers.Input(shape=input_shape)
+
     base_model = keras.applications.resnet50.ResNet50(include_top=False,
                                                       weights='imagenet',
-                                                      input_shape=input_shape)(input)
+                                                      input_shape=input_shape)
     
+    if freeze_weights:
+        for layer in base_model.layers:
+            layer.trainable = False
+
+    input = keras.layers.Input(shape=input_shape)
+    base = base_model(input)
     #TODO make number of layers adjustable for hyper parameters
-    pooling_layer = keras.layers.GlobalAveragePooling2D()(base_model)
+    pooling_layer = keras.layers.GlobalAveragePooling2D()(base)
     hidden_1 = keras.layers.Dense(1000, activation='relu')(pooling_layer)
     output = keras.layers.Dense(10, activation='softmax')(hidden_1)
 
