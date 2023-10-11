b4f0c884a9462dc04248dbc98d3a67f26476b1e1
DIGITALSTORM <judethomas0403@gmail.com>
2023-10-11 00:19:15 -0700

    testing cerug (again)

--- Code and Experiments/modules/qdanalysis/qdanalysis/models.py
+++ Code and Experiments/modules/qdanalysis/qdanalysis/models.py
@@ -5,7 +5,7 @@ methods that build keras models
 import tensorflow as tf
 import tensorflow.keras as keras
 
-def adapt_resnet50(input_shape, freeze_weights=True):
+def adapt_resnet50(input_shape, num_classes=10, freeze_weights=True):
 
     base_model = keras.applications.resnet50.ResNet50(include_top=False,
                                                       weights='imagenet',
@@ -20,6 +20,6 @@ def adapt_resnet50(input_shape, freeze_weights=True):
     #TODO make number of layers adjustable for hyper parameters
     pooling_layer = keras.layers.GlobalAveragePooling2D()(base)
     hidden_1 = keras.layers.Dense(1000, activation='relu')(pooling_layer)
-    output = keras.layers.Dense(10, activation='softmax')(hidden_1)
+    output = keras.layers.Dense(num_classes, activation='softmax')(hidden_1)
 
     return keras.Model(inputs=input, outputs=output)
