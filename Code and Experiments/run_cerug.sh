e2f481e9f8999eedc79230c0a1bb918a43082149
DIGITALSTORM <judethomas0403@gmail.com>
2024-02-21 16:12:34 -0800

    working on CERUG experiments

--- Code and Experiments/run_cerug.sh
+++ Code and Experiments/run_cerug.sh
@@ -4,6 +4,9 @@ echo "starting CERUG experiments!"
 
 rootdir=experiments/CERUG/$(date +%Y_%m_%d)
 echo "root directory:" $rootdir
+
+#TEMP (probably) for development
+rm -rf $rootdir
 mkdir -p $rootdir
 
 dataset_fp="./prepared datasets/CERUG"
@@ -12,14 +15,15 @@ echo "dataset location:" $(realpath -s "$dataset_fp")
 echo "running experiment 1 (train on chinese and test on english)"
 experiment_fp=$rootdir/experiment_1
 mkdir $experiment_fp
-echo "test program. delete later" | tee $experiment_fp/log.txt
+#2&>1 means to redirect stderr to stdout. to print both errors and standard output
+python training.py "$experiment_fp" "$dataset_fp" CN | tee $experiment_fp/log.txt
 
 echo "running experiment 2 (train on english and test on chinese)"
 experiment_fp=$rootdir/experiment_2
 mkdir $experiment_fp
-echo "experiment 2 log" | tee $experiment_fp/log.txt
+python training.py "$experiment_fp" "$dataset_fp" EN | tee $experiment_fp/log.txt
 
 echo "running experiment 3 (train on chinese and english and train on MIXED)"
 experiment_fp=$rootdir/experiment_3
 mkdir $experiment_fp
-echo "TODO..." | tee $experiment_fp/log.txt
+python training.py "$experiment_fp" "$dataset_fp" CN EN | tee $experiment_fp/log.txt
--- Code and Experiments/training.py
+++ Code and Experiments/training.py
@@ -5,35 +5,11 @@ from qdanalysis.models import adapt_resnet50
 import tensorflow as tf
 import tensorflow.keras as keras
 
-def train_model(root_dir: str, train_set, validation_set):
+def train_model(root_dir: str, writer_classes, train_set, validation_set):
 
     #Callback Functions
     #Checkpoint Callback - model directory
     #Tensorboard Callback - logging directory
-    
-    #data loading
-    label_mode = 'categorical'
-    #shape image will be resized to (figure out ideal values later)
-    input_shape = (128, 128, 3)
-    seed = 42
-    validation_split = 0.2
-
-    #due to nature of data, the test and train splits are defined beforehand
-    train_data = keras.preprocessing.image_dataset_from_directory(train_set_fp, 
-                                                                label_mode=label_mode, 
-                                                                image_size=input_shape[:2], 
-                                                                seed=seed, 
-                                                                validation_split=validation_split, 
-                                                                subset='training')
-    train_classes = train_data.class_names
-
-    validation_data = keras.preprocessing.image_dataset_from_directory(train_set_fp,
-                                                                   label_mode=label_mode,
-                                                                   image_size=input_shape[:2],
-                                                                   seed=seed,
-                                                                   validation_split=validation_split,
-                                                                   subset='validation')
-    train_classes_val = validation_data.class_names
 
     #check if train and validationsets have the same writer class
     assert train_classes == train_classes_val, "training set and validation set have different labels"
@@ -62,5 +38,74 @@ def train_model(root_dir: str, train_set, validation_set):
     #model and training
     freeze_weights = False
     model = adapt_resnet50(input_shape, len(writer_classes), False)
+    model.compile(optimizer=optimizer, loss=lossfn, metrics=metrics)
 
+    epochs = 1
+    history = model.fit(train_data, epochs=epochs, validation_data=validation_data)
+
+if __name__ == '__main__':
+    import sys
+    import os
+
+    print("starting experiment")
+
+    #seed for validation splits
+    seed = int.from_bytes(os.urandom(4), 'big')
+    print("experiment seed: ", seed)
+
+    args = sys.argv
     
+    #output of experiment
+    experiment_dir = args[1]
+    print("model weights, logs, and tensorboard files will be stored in ", experiment_dir)
+    #dataset path should be second parameter
+    dataset_fp = args[2]
+
+    if len(args) == 4:
+        print("training with no merging")
+
+        #specified script should be 2 (and above for the merge case)
+        script = args[3]
+
+        train_path = os.path.join(dataset_fp, script)
+
+        print("attempting to load ", train_path)
+        #data loading
+        label_mode = 'categorical'
+        #shape image will be resized to (figure out ideal values later)
+        input_shape = (128, 128, 3)
+        validation_split = 0.2
+
+        #data loading - standard validation split
+        #due to nature of data, the test and train splits are defined beforehand
+        train_data = keras.preprocessing.image_dataset_from_directory(train_path, 
+                                                                    label_mode=label_mode, 
+                                                                    image_size=input_shape[:2], 
+                                                                    seed=seed, 
+                                                                    validation_split=validation_split, 
+                                                                    subset='training')
+        train_classes = train_data.class_names
+
+        validation_data = keras.preprocessing.image_dataset_from_directory(train_path,
+                                                                    label_mode=label_mode,
+                                                                    image_size=input_shape[:2],
+                                                                    seed=seed,
+                                                                    validation_split=validation_split,
+                                                                    subset='validation')
+        train_classes_val = validation_data.class_names
+
+        #check if train and validationsets have the same writer class
+        assert train_classes == train_classes_val, "training set and validation set have different labels"
+
+        print("classes: ", train_classes)
+
+        print("\n") # for clarity in log file
+        
+        train_model(experiment_dir, train_classes, train_data, validation_data)
+
+    elif len(args) > 4:
+        #TODO
+        print("training with merging")
+        print(args)
+    else:
+        print('no dataset file arguments or script descriptors', file=sys.stderr)
